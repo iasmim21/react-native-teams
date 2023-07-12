0925dda22f495bb7631a04a19f56c3d7821e5892
Iasmim Bitencourt <iasmim.bit@gmail.com>
2023-07-11 21:04:46 -0300

    feat (App): use theme provider

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
+import { Loading } from '@components/Loading';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/theme';
 
 export default function App() {
-  return (
-    <Groups />
-  );
+	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+	return (
+		<ThemeProvider theme={theme}>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+
+			{ fontsLoaded ? <Groups /> : <Loading /> }
+		</ThemeProvider>
+	);
 }
