93c64b04c20b26de429e360c36e4b21887ad6d71
Iasmim Bitencourt <iasmim.bit@gmail.com>
2023-07-17 20:23:06 -0300

    style: code improvements

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,5 @@
 import { TextInput } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
 export const Container = styled(TextInput)`
     flex: 1;
@@ -10,9 +10,11 @@ export const Container = styled(TextInput)`
     border-radius: 6px;
     padding: 16px;
 
-    background-color: ${({ theme }) => theme.COLORS.GRAY_700};
-    color: ${({ theme }) => theme.COLORS.WHITE};
+    ${({ theme }) => css`
+        background-color: ${theme.COLORS.GRAY_700};
+        color: ${theme.COLORS.WHITE};
 
-    font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
-    font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
+        font-family: ${theme.FONT_FAMILY.REGULAR};
+        font-size: ${theme.FONT_SIZE.MD}px;
+    `};
 `;
