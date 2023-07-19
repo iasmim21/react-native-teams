976aca93a54da82ebfc4e4a2de36029c57aae954
Iasmim Bitencourt <iasmim.bit@gmail.com>
2023-07-18 21:00:39 -0300

    style: use SafeAreaView

--- src/screens/NewGroup/styles.ts
+++ src/screens/NewGroup/styles.ts
@@ -1,13 +1,14 @@
 import { UsersThree } from 'phosphor-react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.GRAY_600};
     padding: 24px;
 `;
 
-export const Content = styled.View`
+export const Content = styled(SafeAreaView)`
     flex: 1;
     justify-content: center;
 `;
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,21 +1,25 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
 import styled, { css } from 'styled-components/native';
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
     flex: 1;
     background-color: ${({ theme }) => theme.COLORS.GRAY_600};
     padding: 24px;
 `;
 
-export const Form = styled.View`
+export const Form = styled(SafeAreaView)`
     width: 100%;
     background-color: ${({ theme }) => theme.COLORS.GRAY_700};
 
+    padding-top: -30px;
+
     flex-direction: row;
     justify-content: center;
+
     border-radius: 6px;
 `;
 
-export const HeaderList = styled.View`
+export const HeaderList = styled(SafeAreaView)`
     width: 100%;
     flex-direction: row;
     align-items: center;
